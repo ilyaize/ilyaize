ca591841d8f27b5b356636c81096bc889e31e155
Ilya Aizenberg <ilya86berg@gmail.com>
2023-12-22 04:42:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppHeader from "./components/AppHeader.vue";
+import AppFooter from "./components/AppFooter.vue";
+</script>
+
+<template>
+  <AppHeader msg="ilyaize" />
+  <router-view></router-view>
+  <AppFooter />
+</template>
+
+<style scoped></style>
